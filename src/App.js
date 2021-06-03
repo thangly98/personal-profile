b3b7e16a96bfff29e9e90c4071db8f2cd78dc7ee
thangly98 <lqthang98@gmail.com>
2021-06-03 17:40:26 +0700

    [update] data & ui

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
 import React, { Component } from 'react'
 import ReactGA from 'react-ga'
-import $ from 'jquery'
 import './App.css'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
 import About from './Components/About'
 import Resume from './Components/Resume'
 import Contact from './Components/Contact'
-import Testimonials from './Components/Testimonials'
+// import Testimonials from './Components/Testimonials'
 import Portfolio from './Components/Portfolio'
+
+import data from './Data/resumeData.json'
 
 class App extends Component {
     constructor(props) {
         super(props)
         this.state = {
             foo: 'bar',
-            resumeData: {},
+            resumeData: data,
         }
 
         ReactGA.initialize('UA-110570651-1')
         ReactGA.pageview(window.location.pathname)
-    }
-
-    getResumeData() {
-        fetch('resumeData.json', {
-            headers: {
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            },
-        })
-            .then(function (response) {
-                this.setState({ resumeData: response.json() })
-            })
-            .then(function (myJson) {
-                console.log(myJson)
-            })
-        // $.ajax({
-        //   url:'/resumeData.json',
-        //   dataType:'json',
-        //   cache: false,
-        //   success: function(data){
-        //     this.setState({resumeData: data});
-        //   }.bind(this),
-        //   error: function(xhr, status, err){
-        //     console.log(err);
-        //     alert(err);
-        //   }
-        // });
-    }
-
-    componentDidMount() {
-        this.getResumeData()
     }
 
     render() {
@@ -60,7 +30,7 @@ class App extends Component {
                 <About data={this.state.resumeData.main} />
                 <Resume data={this.state.resumeData.resume} />
                 <Portfolio data={this.state.resumeData.portfolio} />
-                <Testimonials data={this.state.resumeData.testimonials} />
+                {/* <Testimonials data={this.state.resumeData.testimonials} /> */}
                 <Contact data={this.state.resumeData.main} />
                 <Footer data={this.state.resumeData.main} />
             </div>
